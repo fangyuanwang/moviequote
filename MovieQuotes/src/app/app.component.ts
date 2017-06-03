363a6cf2cfdd0efbf09cc079c128c3a8beae5469
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-06-03 15:50:57 -0400

    finished local only MQ

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -11,14 +11,22 @@ interface MovieQuote {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  movieQuote: MovieQuote = {
+  formMovieQuote: MovieQuote = {
     'movie': '',
     'quote': ''
   };
 
+  movieQuotes: MovieQuote[] = [
+    {"movie": "Rocky", "quote": "Yo Adrian"},
+    {"movie": "Terminator", "quote": "I'll be back"},
+    {"movie": "Titanic", "quote": "I'm the king of the world!"},
+    {"movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die."}
+  ];
+
+
   onSubmit(): void {
-    console.log("submit", this.movieQuote);
-    this.movieQuote = {
+    this.movieQuotes.unshift(this.formMovieQuote);
+    this.formMovieQuote = {
       'movie': '',
       'quote': ''
     };
